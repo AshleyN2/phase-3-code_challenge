d2aadf6439e4a34041274ef03b48a116581217b7
Ashley <ashleynjeri7@gmail.com>
2022-11-04 08:47:35 +0300

    updated to test

--- tools/console.rb
+++ tools/console.rb
@@ -4,16 +4,27 @@ def reload
   load 'config/environment.rb'
 end
 
-
 ###  WRITE YOUR TEST CODE HERE ###
 
+#Author
+author1 = Author.new("Naval Ravikant")
+author2 = Author.new("Biko Zulu")
 
+#Magazine
+lifestyle = Magazine.new("Exercise", "Yoga")
+interior = Magazine.new("Music room", "Rustic")
+technology = Magazine.new("Innovation", "AI")
 
-
-
-
-
-
+#Article
+article1 = Article.new(author1, lifestyle, "Issue No.1")
+article2 = Article.new(author2, interior, "Issue No.1")
+article2 = Article.new(author1, technology, "Issue No.1")
+article4 = Article.new(author2, lifestyle, "Issue No.2")
+article5 = Article.new(author1, interior, "Issue No.2")
+article6 = Article.new(author2, technology, "Issue No.2")
+article7 = Article.new(author1, lifestyle, "Issue No.3")
+article8 = Article.new(author2, interior, "Issue No.3")
+article9 = Article.new(author1, technology, "Issue No.3")
 
 ### DO NOT REMOVE THIS
 binding.pry
